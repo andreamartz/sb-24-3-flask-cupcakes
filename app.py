c3defc82fb9d974391a2e66ebc6471b081897d3f
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-09-19 20:58:09 -0500

    Set up app config and imports

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+connect_db(app)
+db.create_all()
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
