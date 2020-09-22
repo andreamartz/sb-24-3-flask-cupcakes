70cfb8cb53cd86be62442ca46f8d950702202f7c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-09-22 14:53:05 -0500

    Fix a typo and some code indentations

--- forms.py
+++ forms.py
@@ -9,11 +9,12 @@ from wtforms.validators import Optional, InputRequired, AnyOf, NumberRange, URL
 class AddCupcakeForm(FlaskForm):
     """Form for adding cupcakes"""
     flavor = StringField(label="Flavor", validators=[
-                         InputRequired(message="Flavore cannot be blank.")])
+                         InputRequired(message="Flavor cannot be blank.")])
     size = SelectField(label="Size", choices=[('small', 'small'), ('medium', 'medium'), ('large', 'large')], validators=[InputRequired(
         message="Size cannot be blank."), AnyOf(['small', 'medium', 'large'], message="Size must be small, medium, or large.")])
-    rating = DecimalField(label="Rating", places=1, validators=[
-                          InputRequired(message="Rating cannot be blank."), NumberRange(min=0, max=10, message="Rating must be between 0 and 10.")])
+    rating = DecimalField(label="Rating", places=1,
+                          validators=[
+                              InputRequired(message="Rating cannot be blank."), NumberRange(min=0, max=10, message="Rating must be between 0 and 10.")])
     image = StringField(label="Photo URL", validators=[
         Optional(),
         URL(require_tld=True, message="URL must be valid.")])
